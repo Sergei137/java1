6c33639df80f1ae22a65b6caf881c54029d057bb
Sam <sm1.618@protonmail.com>
2023-07-15 18:27:31 -0600

    updated create date, added simple math

--- hello.java
+++ hello.java
@@ -6,7 +6,16 @@
 public class hello {
     public static void main(String[] args) {
         System.out.println("Hello world");
+
+        int a = 10;
+        int b = 20;
+        int c = 0;
+
+        c = a + b;
+
+        System.out.println(c);
     }
 }
+
 
 
